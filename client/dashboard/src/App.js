39bada3d35eee19414c486f223c7dc6043787b96
Dadoright <donabusi1@gmail.com>
2022-01-12 09:33:39 +0530

    Sidebar is added

--- client/dashboard/src/App.js
+++ client/dashboard/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import Home from './Pages/Home';
 import Users from './Pages/Users';
 import MoUs from './Pages/MoUs';
 import ErrorPage from './Pages/ErrorPage';
 
+import Sidebar from './Component/Sidebar';
+
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/users"> Users </Link>
-        <Link to="/mous"> MoUs </Link>
-      </nav>
+      <Sidebar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
---
+++ client/dashboard/src/Component/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '../Styles/sidebar.css';
+import { Link } from 'react-router-dom';
+
+import { AiFillHome } from 'react-icons/ai';
+import { RiPagesLine } from 'react-icons/ri';
+import { FaUsers } from 'react-icons/fa';
+
+function Sidebar() {
+  return (
+    <div class="sidebar">
+      <ul>
+        <li>
+          <Link to="/">
+            <AiFillHome />
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/mous">
+            <RiPagesLine />
+            MoUs
+          </Link>
+        </li>
+        <li>
+          <Link to="/users">
+            <FaUsers />
+            Users
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
