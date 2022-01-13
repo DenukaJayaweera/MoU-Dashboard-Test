d3963958f5abf2a31d394c4d493897c695eec878
Dadoright <donabusi1@gmail.com>
2022-01-13 07:15:36 +0530

    adding a server side

--- client/dashboard/src/App.js
+++ client/dashboard/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './Pages/Home';
