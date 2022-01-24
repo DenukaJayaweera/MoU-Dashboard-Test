e931c590bf3849ac154135b5dc380dfe83dba978
Dadoright <donabusi1@gmail.com>
2022-01-24 18:27:38 +0530

    Rendered the country list

--- client/dashboard/src/Tables/CountryTable.js
+++ client/dashboard/src/Tables/CountryTable.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 function CountryTable() {
+  const [countryList, setCountryList] = useState([]);
+
+  const getCountry = () => {
+    Axios.get(`${global.url}/country`).then((response) => {
+      setCountryList(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getCountry();
+  });
+
+  const renderCountryList = (val, key) => {
+    return (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{val.CountryCode}</td>
+        <td>{val.CountryName}</td>
+      </tr>
+    );
+  };
+
   return (
     <div>
-      <h1>This is the Country Table</h1>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Code</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>{countryList.map(renderCountryList)}</tbody>
+      </table>
     </div>
   );
 }
